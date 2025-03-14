34fef92162554a47746a766c98cc06268cf3acf0
Dr BAA <muchiribeth@yahoo.co.uk>
2025-03-14 08:50:38 +0000

    completed lesson 032 - while loops

--- 032_while_loops.py
+++ 032_while_loops.py
@@ -4,7 +4,7 @@ from lib.helpers import check_that_these_are_equal
 
 # Remember `if`s? Here's a reminder:
 
-my_name = "Kay"
+my_name = "Beth"
 if my_name == "Kay":
   print("Hello, Kay!")
 else:
@@ -20,9 +20,10 @@ else:
 # A while loop is perhaps the most simple:
 
 i = 0 # We call this the counter variable
-while i < 10:
+while i <= 10:
   print(f"The number is now {i}")
-  i = i + 1
+  # i = i + 2
+  i += 1
 
 # @TASK: run this program and see what it does.
 
@@ -38,15 +39,48 @@ while i < 10:
 print("")
 print("Function: add_cats_repeatedly")
 
+
+
 # Write a function that adds the item "cats" to the given word_list, repeatedly,
 # a number of times defined by the given count parameter.
 # Example:
 #    add_cats_repeatedly([], 3)
 # => ['cats', 'cats', 'cats']
 
+# ORIGINAL CODE 
 def add_cats_repeatedly(word_list, count):
   # ...
   return word_list
+
+# CODE NOT WORKING FOR THE SECOND TEST
+# in this example, The count parameter represents the number of times we want to add "cats" to the word_list. We do not need to reset the count to 0.
+# By resetting it to 0, we lose the user's original input (e.g., 3). Instead, we use the count value as a target and compare it to the length of word_list to determine how many "cats" need to be added.
+# the number of iterations is determined indirectly by the growing length of word_list, so a separate counter variable isn't necessary. If the user calls add_cats_repeatedly([], 3), the word_list starts empty (len(word_list) = 0). the while loop checks whether the length of word_list has reached the target value of count. By appending "cats" in each iteration, the length of word_list increases automatically, so we don't need to increment count.
+
+print ("CODE NOT WORKING FOR THE SECOND TEST \n")
+# def add_cats_repeatedly(word_list, count):
+#     while len(word_list) < count:
+#         word_list.append("cats")
+#     return word_list
+
+# check_that_these_are_equal(
+#   add_cats_repeatedly([], 3), ['cats', 'cats', 'cats'])
+# check_that_these_are_equal(
+#   add_cats_repeatedly(['dogs'], 2), ['dogs', 'cats', 'cats'])
+
+# NEW CODE - WORKING WITH COPILOT HELP
+# Copilot introduced a counter variable (cats_added) to track how many times "cats" has been appended to word_list.
+# The while loop runs until the total number of "cats" added (cats_added) equals the specified count.
+# This approach doesn't rely on the initial length of word_list, so it works correctly for test cases where the list already has other items.
+
+print("NEW CODE - WORKING WITH COPILOT HELP \n")
+def add_cats_repeatedly(word_list, count):
+    cats_added = 0  # Tracks how many "cats" have been added
+    while cats_added < count:
+        word_list.append("cats")
+        cats_added += 1  # Increment the counter
+    return word_list
+
 
 check_that_these_are_equal(
   add_cats_repeatedly([], 3), ['cats', 'cats', 'cats'])
