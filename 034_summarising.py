0a60651668a09ffba710b3b11e0c58117181c7f4
Dr BAA <muchiribeth@yahoo.co.uk>
2025-03-14 20:50:07 +0000

    completed lesson 034 - summarising for loops

--- 034_summarising.py
+++ 034_summarising.py
@@ -40,7 +40,8 @@ print(text)
 
 another_text = "\n".join(lines)
 # Uncomment this next line if you want to see it:
-# print(another_text)
+print("\nusing function to join lines from a list of strings")
+print(another_text)
 
 # `join` is actually little smarter — it only adds the `\n` character between
 # lines, not at the end also.
@@ -53,10 +54,27 @@ print("Function: add_up_numbers")
 # Add up all the numbers in the list
 def add_up_numbers(numbers):
   pass
+  total = 0  # Initialize a running total
+  for number in numbers:
+      total += number  # Add each number to the running total
+  return total  # Return the final total
 
 check_that_these_are_equal(
   add_up_numbers([1, 2, 3, 4]), 10)
 check_that_these_are_equal(
   add_up_numbers([2, 3, 4, 5]), 14)
+
+
+# Function: Add up all the numbers in the list
+def add_up_numbers2(numbers):
+    total = 0  # Initialize a running total
+    for number in numbers:
+        print("this number is: ", number)
+        total += number  # Add each number to the running total
+    return total  # Return the final total
+
+print("Calling the function and printing the result")
+result = add_up_numbers2([1, 2, 3, 4])  # Passing a list of numbers
+print("TOTAL = ", result)  # Output: 10
 
 # When you're done, move on to 035_mapping.py
